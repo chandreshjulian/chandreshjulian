b3e2742b855bf25db2cbaed6d78f3ed9aeef6d24
Chandresh Gurbani <chandreshjulian@gmail.com>
2022-03-20 16:15:22 +0530

    details added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const path = require("path");
+
 const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname, "./client/index.html"))
 })
 
 app.listen(port, () => {
